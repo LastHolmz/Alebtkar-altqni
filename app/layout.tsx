204563ac7852511d3598b78117cfb0720381a253
hodifa <databaserepos@gmail.com>
2024-10-12 21:31:21 +0200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,24 @@ export const metadata: Metadata = {
     "شركة تقنية",
     "أنظمة إدارة",
   ],
+  openGraph: {
+    type: "website",
+    url: "https://alebtkar-altqni.vercel.app",
+    title: "شركة الإبتكار التقني | ابتكار البرمجيات",
+    description:
+      "شركة الإبتكار التقني توفر حلول برمجية مبتكرة لتطوير مواقع الإنترنت، تطبيقات الجوال، وأنظمة نقاط البيع.",
+    images: [
+      {
+        url: "https://alebtkar-altqni.vercel.app/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "شركة الإبتكار التقني logo",
+      },
+    ],
+    siteName: "شركة الإبتكار التقني",
+  },
 };
+
 const cairo = Cairo({
   weight: ["200", "1000", "500", "400", "700", "300", "600"],
   subsets: ["arabic"],
@@ -40,7 +57,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="ar" dir="rtl" suppressHydrationWarning>
-        <head />
         <body className={cn(cairo.className)}>
           <ThemeProvider
             attribute="class"
